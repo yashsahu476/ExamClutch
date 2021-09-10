39bd612380900c1004a2157af4eea80632a17411
Hritik Soni <56077574+iamhritik55@users.noreply.github.com>
2021-09-10 15:33:09 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div align='center'>
+      <h4>Software Engineering Project | Exam Clutch</h4>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
